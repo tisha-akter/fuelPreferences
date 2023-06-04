b62fdd433a6b1718b7f1a4c31517246e9a180f3f
tisha-akter <tishaakter.jui79@gmail.com>
2023-06-04 22:57:48 +0600

    replaced variable name interface to rl

--- fuelPreferences.js
+++ fuelPreferences.js
@@ -3,20 +3,20 @@ const readline = require('readline');
 function countFuelPreferences() {
   const fuelTypes = ['Alcohol', 'Gasoline', 'Diesel'];
   const fuelCounts = [0, 0, 0];
-  const interface = readline.createInterface({
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
   function promptFuelType() {
-    interface.question('Enter the fuel type (1. Alcohol, 2. Gasoline, 3. Diesel, 4. End): ', (answer) => {
+    rl.question('Enter the fuel type (1. Alcohol, 2. Gasoline, 3. Diesel, 4. End): ', (answer) => {
       const fuelType = parseInt(answer);
 
       if (fuelType >= 1 && fuelType <= 3) {
         fuelCounts[fuelType - 1]++;
         promptFuelType();
       } else if (fuelType === 4) {
-        interface.close();
+        rl.close();
         printFuelPreferences();
       } else {
         console.log('Invalid code. Please enter a valid fuel type.');
